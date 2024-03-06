8e9e9d8971259290a72b08c99f7f0a9e56584744
muhammed-ameen-s <149754122+muhammed-ameen-s@users.noreply.github.com>
2024-03-06 01:01:28 -0800

    demo java file added

---
+++ demo.java
@@ -0,0 +1,15 @@
+class demo 
+{
+  public static void main(String[] args)
+{
+    human hm = new human();
+    hm.setage(19);
+    hm.setname("asd");
+    hm.setmail("adsgsg@132.gamil.com");
+    hm.setnumber(1234567890);
+    System.out.println(hm.getage());
+    System.out.println(hm.getname());
+    System.out.println(hm.getmail());
+    System.out.println(hm.getnumber());
+}
+}
