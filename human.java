78990542c62c9bb7f40db0b080a1975e90466122
muhammed-ameen-s <149754122+muhammed-ameen-s@users.noreply.github.com>
2024-03-06 01:01:00 -0800

    java file added

---
+++ human.java
@@ -0,0 +1,37 @@
+class human {
+  private int age;
+  private String name;
+  private String mail;
+  private int number;
+
+  public int getage() {
+    return age;
+  }
+
+  public void setage(int age) {
+    this.age = age;
+  }
+
+  public String getname() {
+    return name;
+  }
+  public void setname(String name) {
+    this.name = name;
+  }
+
+  public String getmail() {
+    return mail;
+  }
+
+  public void setmail(String mail) {
+    this.mail = mail;
+  }
+
+  public int getnumber() {
+    return number;
+  }
+
+  public void setnumber(int number) {
+    this.number = number;
+  }
+}
